a4de004841038bd0db5de881f896437f99cd0ba0
SDH <unfade2@gmail.com>
2021-04-26 17:00:36 +0900

    1. DTO alias 패키지 단위로 설정 2. 도서 관리에서 책 리스트 불러오기 하는중

--- src/main/java/com/spring/main/controller/BookController.java
+++ src/main/java/com/spring/main/controller/BookController.java
@@ -1,5 +1,29 @@
 package com.spring.main.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.main.service.BookService;
+
+@Controller
 public class BookController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired BookService service;
+	
+	@RequestMapping(value = "/normalBookManage", method = RequestMethod.GET)
+	public ModelAndView normalBookManage(Model model) {
+		logger.info("일반도서 리스트");
+		
+		//"/BookManage/normalBookManage"
+		return service.bookManageList();
+	}
+	
 }
---
+++ src/main/java/com/spring/main/dto/BookDTO.java
@@ -0,0 +1,68 @@
+package com.spring.main.dto;
+
+import java.sql.Date;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("book")
+public class BookDTO {
+
+	private int bookIdx;
+	private String bookName;
+	private String bookImg;
+	private String writer;
+	private String story;
+	private String publisher;
+	private Date reg_date ;
+	private String bookState;
+	
+	public int getBookIdx() {
+		return bookIdx;
+	}
+	public void setBookIdx(int bookIdx) {
+		this.bookIdx = bookIdx;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookImg() {
+		return bookImg;
+	}
+	public void setBookImg(String bookImg) {
+		this.bookImg = bookImg;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getStory() {
+		return story;
+	}
+	public void setStory(String story) {
+		this.story = story;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getBookState() {
+		return bookState;
+	}
+	public void setBookState(String bookState) {
+		this.bookState = bookState;
+	}
+	
+}
