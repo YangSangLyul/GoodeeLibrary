02d35acde874c5bb23b95bc6a0881860fe4b0233
DESKTOP-T4SP14R\HSK <stolenblue10813@gmail.com>
2021-04-28 18:11:07 +0900

    1. 좌석 예약 구현중.. ( 현재 좌석 리스트 불러오기 구현중 )

--- src/main/java/com/spring/main/controller/SeatController.java
+++ src/main/java/com/spring/main/controller/SeatController.java
@@ -2,20 +2,26 @@ package com.spring.main.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.main.service.reserveSeatService;
 
 @Controller
 public class SeatController {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
+	@Autowired reserveSeatService reserve;
+	
 	@RequestMapping(value = "/reserveSeatForm", method = RequestMethod.GET)
-	public String home(Model model) {
+	public ModelAndView reserveSeat() {
 		
 		logger.info("도서관 서비스 메뉴(좌석 예약) 진입");
-		return "reserveSeatForm";
+		return reserve.reserveSeatList();
 	}
 }
